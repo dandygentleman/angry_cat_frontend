2059677cbfabac297423daaa1274ed356c8ecdaf
barryjung <wjdgusrbs5@gmail.com>
2023-05-25 11:32:52 +0900

    refactor : Edit signup & login
    로그인 처리를 약간 수정했습니다.
    회원가입, 로그인 실패시 status코드를 alert하게 했습니다.

--- scripts/api.js
+++ scripts/api.js
@@ -100,22 +100,7 @@ async function handleLogin() {
         })
     })
 
-    if(response.status == 200){
-        const response_json = await response.json()
-
-
-        localStorage.setItem("access", response_json.access);
-        localStorage.setItem("refresh", response_json.refresh);
-        const jsonPayload = await parse_payload(response_json.access);
-
-        localStorage.setItem("payload", jsonPayload);
-        alert("환영합니다.")
-        window.location.replace(`${frontend_base_url}/`)
-    }else{
-        alert("회원정보가 일치하지 않습니다.")
-    }
-
-
+    return response
 }
 
 
--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,20 @@
+async function handleLoginButton(){
+    const response = await handleLogin();
+
+    if(response.status==200){
+        const response_json = await response.json()
+
+        localStorage.setItem("access", response_json.access);
+        localStorage.setItem("refresh", response_json.refresh);
+        const jsonPayload = await parse_payload(response_json.access);
+
+        localStorage.setItem("payload", jsonPayload);
+        alert("환영합니다.")
+        window.location.replace(`${frontend_base_url}/`)
+    }else{
+        alert(response.status)
+    }
+}
+
+
 checkLogin()
